2f23737b36d80cea942b8ffe4cebde4f80568b72
kunal-c <erkunal0318@gmail.com>
2016-09-22 16:07:00 +0530

    github commit 2

--- Git-Demo/src/com/git/Demo1.java
+++ Git-Demo/src/com/git/Demo1.java
@@ -4,5 +4,6 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Welcome to Java!!!");
+		System.out.println("Start learning Git");
 	}
 }
