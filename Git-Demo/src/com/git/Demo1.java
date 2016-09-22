bc60402b9401cd7088d610866156eb0ffd367088
kunal-c <erkunal0318@gmail.com>
2016-09-22 16:03:15 +0530

    github commit

--- Git-Demo/src/com/git/Demo1.java
+++ Git-Demo/src/com/git/Demo1.java
@@ -3,6 +3,6 @@ package com.git;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
-		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java!!!");
 	}
 }
